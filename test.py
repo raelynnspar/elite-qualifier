2b343183c57679258e94de1bd2a76c698edd4129
Katie Sparr <sparr.katier@gmail.com>
2021-02-11 23:38:01 +0000

    Fixed test file

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import unittest
 
-class MyTests(unittest.TestCase)
+class MyTests(unittest.TestCase):
 
-def test_response(self){
-  self.assertTrue(True)
+  def test_response(self):
+    self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
   
-}
+
